be83d95e55753960511a472f16702b4d969fb86f
Snoozebugs92 <100491452+Snoozebugs92@users.noreply.github.com>
2022-09-11 15:03:43 +0200

    First fields and properties added.

---
+++ AdressBookApp/AdressBook/Info/PersonAddres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook.Info
+{
+    public class PersonAddres : PersonBase
+    {
+        private readonly string _street;
+        private readonly string _city;
+        private readonly string _state;
+        private readonly string _zip;
+
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ AdressBookApp/AdressBook/Info/PersonContact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook.Info
+{
+    public class PersonContact : PersonAddres
+    {
+        private readonly string? _email;
+        private readonly string? _phone;
+        private readonly string? _workPhone;
+        private readonly string? _linkedin;
+        private readonly string? _instagram;
+        private readonly string? _youtube;
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Linkedin { get; set; }
+        public string Instagram { get; set; }
+        public string Youtube { get; set; }
+    }
+}
